8f79c06986f727863e2694776de4c34bd6249bf5
Brett Parker <absentminded.lich@gmail.com>
2022-11-09 11:54:31 -0500

    Complete step 1

--- calculator.js
+++ calculator.js
@@ -1,3 +1,10 @@
+let add = (addend1, addend2) => addend1 + addend2;
+let subtract = (minuend, subtrahend) => minuend - subtrahend;
+let multiply = (multiplier, multiplicand) => multiplier * multiplicand;
+let divide = (dividend, divisor) => {
+    if (divisor === 0) return undefined;
+    return dividend / divisor;
+}
 // add
 // subtract
 // multiply
