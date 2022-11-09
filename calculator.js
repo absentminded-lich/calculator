aafa969672e629d765f2fc0577d7cc83be4eda5d
Brett Parker <absentminded.lich@gmail.com>
2022-11-09 12:07:26 -0500

    Complete step 2

--- calculator.js
+++ calculator.js
@@ -5,11 +5,16 @@ let divide = (dividend, divisor) => {
     if (divisor === 0) return undefined;
     return dividend / divisor;
 }
-// add
-// subtract
-// multiply
-// divide
-// operate(operator, a, b)
+let operate = (operator, num1, num2) => {
+    if (typeof operator === 'function') return operator(num1, num2); // just because
+    switch (operator) {
+        case '+': return add(num1, num2);
+        case '-': return subtract(num1, num2);
+        case '*': return multiply(num1, num2);
+        case '/': return divide(num1, num2);
+        default: break;
+    }
+}
 // equals
 // clear
 // output
